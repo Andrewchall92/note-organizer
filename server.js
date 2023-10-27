a538b31231e76b7f0a9f86889dccba0c0a3d62f2
moxnix <andrewchall92@gmail.com>
2023-10-26 22:12:59 -0700

    added the post request and installed 'uuid' to give the notes unique ids

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const notes = require('./db/db.json');
+const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,14 +14,21 @@ app.get('/notes', (req, res) => {
     res.sendFile(__dirname + '/public/notes.html');
 });
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     console.log('hi')
     res.sendFile(__dirname + '/public/index.html');
 });
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
-})
+});
+
+app.post('/api/notes', (req, res) =>{
+    req.body.id = uuidv4();
+    notes.push(req.body);
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes)) 
+    res.json(notes)
+});
 
 app.listen(PORT, () => {
     console.log('hi');
